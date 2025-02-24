acc4a1ce44f88e1edad239a70116bc4ba297aab1
DavitTchintcharauli <dchincharauli@spinoman.com>
2025-02-24 20:28:04 +0400

    update code logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Flowers</Link> <div>🚀 შენი როლი: {role}</div>
+                    <Link href={"/"}>🌺 Flower Shop</Link> <div>🚀 შენი როლი: {role}</div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
